ecd63ecd70fafd663b66ccd3844cff5c9b472deb
ConnorJKY <keyoujiang2023@gmail.com>
2024-09-06 14:44:32 -0400

    Replace Rectangle with Shape to define

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,12 +1,15 @@
 package edu.cmu.cs.cs214.lab02;
 
-import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
-
-        Renderer renderer = new Renderer(rectangle);
+//        Rectangle rectangle = new Rectangle(2, 3);
+//        Circle circle = new Circle(1.5);
+//        Renderer renderer1 = new Renderer(2, 3);
+//        Renderer renderer2 = new Renderer(circle);
+        Shape shape = new Rectangle(2, 3);
+        Renderer renderer = new Renderer(shape);
 
         renderer.draw();
     }
